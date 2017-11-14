ad8a1566e265a3ba6ce4aa23e2cd4dfe23d436f0
Mark Geller <markg@condukia.com>
2017-11-14 09:02:59 -0800

    adding event listeners to player buttons

--- ScoreKeeper/ScoreKeeper.js
+++ ScoreKeeper/ScoreKeeper.js
@@ -1,1 +1,27 @@
-alert('connected');
+var p1Button = document.querySelector("#p1");
+var p2Button = document.getElementById("p2");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var reset = document.querySelector("#reset");
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
+
+p1Button.addEventListener("click", function() {
+  p1Score++;
+  p1Display.textContent = p1Score;
+});
+
+p2Button.addEventListener("click", function() {
+  p2Score++;
+  p2Display.textContent = p2Score;
+})
+
+reset.addEventListener("click", function() {
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = p1Score;
+  p2Display.textContent = p2Score;
+  var gameOver = false;
+})
