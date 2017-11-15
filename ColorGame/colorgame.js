d66055c9e622124b9b155da425b5b1776de711da
mageller <markg@condukia.com>
2017-11-14 17:04:47 -0800

    finish logic and style of color game

--- ColorGame/colorgame.js
+++ ColorGame/colorgame.js
@@ -1,10 +1,62 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
 
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function() {
+  easyBtn.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+  h1.style.backgroundColor = "steelblue";
+})
+
+hardBtn.addEventListener("click", function() {
+  easyBtn.classList.remove("selected");
+  hardBtn.classList.add("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = "block";
+  }
+  h1.style.backgroundColor = "steelblue";
+})
+
+resetButton.addEventListener("click", function() {
+  // generate new colors
+  colors = generateRandomColors(numSquares);
+  // pick a new random color from array
+  pickedColor = pickColor();
+  // change color display to match picked Color
+  colorDisplay.textContent = pickedColor;
+  // change colors of squares
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  // reset display
+  h1.style.backgroundColor = "steelblue";
+  messageDisplay.textContent = "";
+  this.textContent = "New Colors";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -21,6 +73,7 @@ for (var i = 0; i < squares.length; i++) {
       messageDisplay.textContent = "Correct!";
       changeColors(clickedColor);
       h1.style.background = clickedColor;
+      resetButton.textContent = "Play Again?";
     } else {
       this.style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try Again";
